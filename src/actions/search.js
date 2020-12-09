1941360a090efb27dc817be486b37448590f3cb1
Chloe Tan <chloe.tan95@gmail.com>
2020-12-08 20:10:42 -0500

    Almost completed defining action creators. Issue with test that makes sure there is a call to youtube API.

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,17 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+
+  let handleSearch = function() {
+    let action = {
+      'type': 'SEARCH_VIDEO',
+      'query': q,
+    };
+    return action;
+  };
+
+  return handleSearch;
 };
 
 export default handleVideoSearch;
